77a97e3f63cb40c5dbfa7753e5ad43523f448da1
zhaohonglin5 <zhaohonglin1@jd.com>
2020-12-25 15:11:37 +0800

    feature(经典排序算法): 插入排序调整,更容易理解一些

--- src/main/java/com/howliked/algorithm/sort/impl/InsertSort.java
+++ src/main/java/com/howliked/algorithm/sort/impl/InsertSort.java
@@ -6,23 +6,29 @@ import java.util.Arrays;
 
 /**
  * 插入排序(经常碰到这样一类排序问题：把新的数据插入到已经排好的数据列中)
+ * 1. 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
+ * 2. 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
  */
 public class InsertSort implements IArraySort {
     @Override
     public int[] sort(int[] sourceArray) {
-        int[] sortArr = Arrays.copyOf(sourceArray, sourceArray.length);
-        int length = sortArr.length;
-        int insertNum;  //要插入的数
-        //要插入的次数,默认index 为0,已排序,所以从1开始
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+        int length = arr.length;
+        // 从下标为1的元素开始选择合适的位置插入，因为下标为0的只有一个元素，默认是有序的
         for (int i = 1; i < length; i++) {
-            insertNum = sortArr[i];  //要插入的数
-            int j = i - 1;
-            while (j >= 0 & sortArr[j] > insertNum) {
-                sortArr[j + 1] = sortArr[j];
+            int tmp = arr[i]; //要插入的数据
+            // 从已经排序的序列最右边的开始比较，找到比其小的数
+            int j = i;
+            while (j > 0 && tmp < arr[j - 1]) {
+                arr[j] = arr[j - 1];
                 j--;
             }
-            sortArr[j + 1] = insertNum;
+            //存在比其小的数，插入
+            if (j != i) {
+                arr[j] = tmp;
+            }
+
         }
-        return sortArr;
+        return arr;
     }
 }
